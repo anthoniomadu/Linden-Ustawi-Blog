c7354d569a1b23c6797b882749763ee6e85ffbab
anthoniomadu <31852341+anthoniomadu@users.noreply.github.com>
2020-10-07 13:18:39 +0100

    Initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fd84c3c57a56d8fca422932ce08dfb04'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c0a66d35-d33a-4fc7-b4a3-eab3d4042077'
+};
